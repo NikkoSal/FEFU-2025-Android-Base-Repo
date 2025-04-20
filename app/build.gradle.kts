d2ca727fcf6cc7ff8c7a342abf1354d5fc0adea1
Nikita Seliverstov <gamebot202@mail.ru>
2025-04-20 13:25:20 +1000

    task1
    added NewActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,10 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
